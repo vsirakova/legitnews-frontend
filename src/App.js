2d2da2fbbe70ead3c07d6ea3ba3cffd038ad78b6
root <root@eekdev.localdomain>
2017-09-16 13:23:47 +0000

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Routes from './Routes'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import 'babel-polyfill'
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 export default class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {}
     }
-    render() {
-        return (<Routes />)
+    render() { 
+        return (
+		<MuiThemeProvider>
+			<Routes />
+			</MuiThemeProvider>
+			)
     }
 }
