d9b9f218435afadd2adbec583bc7a8f4462236cb
kucukkanat <htolgasahin@gmail.com>
2017-09-16 14:03:55 +0200

    App.js fix

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-import react from 'react'
+import React from 'react'
+import Routes from './Routes'
+
+export default class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (<Routes />)
+    }
+}
